4b3f4b2b64fe72120f72fb8610480f2145ef6ef4
unisys12 <unisys12@gmail.com>
2013-07-20 20:37:37 -0500

    Create home/root page

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,51 @@ angular.module('ResumeApp', []).
 		$locationProvider.html5Mode(true);
 		$routeProvider.
 		when('/Resume', { templateUrl: 'resume/templates/home.html', controller: 'resumeController' }).
+		when('/Resume/About/', { templateUrl: 'resume/templates/about.html', controller: 'aboutController' }).
+		when('/Resume/Projects/', { templateUrl: 'resume/templates/projects.html', controller: 'projectsController' }).
 		otherwise({redirectTo: '/Resume'});
 	});
 
 function resumeController($scope){
+	$scope.libs =
+	[
+		{
+			name : 'Angularjs',
+			url  : 'http://angularjs.org/',
+			ver  : '1.0.7'
+		},
+		{
+			name : 'jQuery',
+			url  : 'api.jquery.com',
+			ver  : '1.10.2'
+		},
+		{
+			name : 'Normalize',
+			url  : 'http://necolas.github.io/normalize.css/',
+			ver  : '2.1.2'
+		}
+	];
+
+	$scope.tools =
+	[
+		{
+			name : 'Sublime Text 2',
+			url  : 'www.sublimetext.com',
+			ver  : '2.0.2'
+		},
+		{
+			name : 'Git',
+			url  : 'http://git-scm.com/',
+			ver  : '1.8.3'
+		}
+	];
+
+}
+
+function aboutController($scope){
+
+}
+
+function projectsController($scope){
 
 }
